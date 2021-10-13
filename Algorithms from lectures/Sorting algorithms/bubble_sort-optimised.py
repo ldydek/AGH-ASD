503ca50ecfdd97c7a5e104c649fdeb523242521c
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-13 19:35:14 +0200

    Update bubble_sort-optimised.py

--- Algorithms from lectures/Sorting algorithms/bubble_sort-optimised.py
+++ Algorithms from lectures/Sorting algorithms/bubble_sort-optimised.py
@@ -3,12 +3,11 @@
 # already sorted. After first iteration the biggest number is at the end, so on its appropriate place. After second
 # iteration second biggest number is on its suitable place and so on. This trick will reduce number of operations, but
 # time complexity is still O(n^2) - in pessimistic case.
-# [PL] Sortowanie bąbelkowe. Elementy w każdej iteracji pętli wewnętrznej są porównywane i gdy element na lewo jest
-# większy od tego na prawo, to są one zamieniane miejscami. W pesymistycznym przypadku najmniejszy element będzie na
-# samym końcu tablicy do posortowania, a zatem po wykonaniu "n" razy pętli zewnętrznej na pewno otrzymamy posortowaną
-# tablicę. Warto zauważyć, iż po każdej iteracji na końcu tablicy tworzy się podzbiór elementów już posortowanych. Po
-# pierwszej będzie to największa liczba, po drugiej będzie to druga największa liczba itd. Ta sztuczka pozwoli
-# zredukować ilość operacji, lecz złożoność algorytmu zostaje nadal O(n^2) - z dokładnością do stałej.
+# [PL] Zoptymalizowana wersja sortowania bąbelkowego. Jeśli chcesz się dowiedzieć, jak działa ten algorytm, to odsyłam
+# do notatek w pliku bubble_sort. Warto tutaj zauważyć, iż po każdej iteracji na końcu tablicy tworzy się
+# podzbiór elementów już posortowanych. Po pierwszej będzie to największa liczba, po drugiej będzie to druga największa 
+# liczba itd. Ta sztuczka pozwoli zredukować ilość operacji, lecz złożoność algorytmu zostaje nadal O(n^2)
+# - z dokładnością do stałej.
 
 def optimised_bubble_sort(tab):
     n = len(tab)
