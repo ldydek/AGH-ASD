086461f070621c7e6e26baa654a1e53b69d764b1
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-14 10:33:32 +0200

    Update counting_sort.py

--- Algorithms from lectures/Sorting algorithms/counting_sort.py
+++ Algorithms from lectures/Sorting algorithms/counting_sort.py
@@ -3,15 +3,15 @@
 # allocate two additional arrays: in the first one (called C) we store information how many elements are less or equal
 # to "i" and we can find that in C[i]. Second one we use simply to rewrite elements and get solution.
 # Note that negative integer numbers are also acceptable - then we have to find equation converting numbers to indexes.
-# Counting sort is stable!
+# Counting sort is stable, but doesn't work in place (two auxiliary arrays).
 # Time complexity: O(n+k) where k is the range of numbers to sort. If k = O(n) time complexity is θ(n).
 # [PL] W algorytmie sortowania przez zliczanie nie dokonujemy żadnych porównań między elementami, dlatego jesteśmy
 # w stanie posortować daną tablicę dla specyficznych danych (liczby całkowite z małego zakresu) w czasie liniowym.
 # Alokujemy dwie pomocnicze tablice. Pierwsza z nich (nazwana C) służy do przechowywania informacji - dla elementu
 # C[i] wiemy, ile liczb jest mniejszych lub równych i. W tablicy B przechowujemy posortowaną tablicę.
 # Złożoność obliczeniowa: O(n+k), gdzie k jest zakresem liczb do posortowania. Jeśli k = O(n), to posortowanie
-# zajmie θ(n). Algorytm jest stabilny! Liczby ujemne też mogą wystąpić i wtedy wystarczy znaleźć odpowiednie równanie
-# przeliczania liczb na indeksy.
+# zajmie θ(n). Algorytm jest stabilny, ale nie działa w miejscu (dwie dodatkowe tablice). Liczby całkowite ujemne też
+# mogą wystąpić i wtedy wystarczy znaleźć odpowiednie równanie przeliczania liczb na indeksy.
 
 def count_sort(A, k):
     n = len(A)
