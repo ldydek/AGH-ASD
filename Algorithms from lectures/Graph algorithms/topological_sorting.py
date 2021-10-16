fc70b34a149b69f58f0f497456c67c526eb9f651
ldydek <91955435+ldydek@users.noreply.github.com>
2021-10-16 19:44:55 +0200

    Update topological_sorting.py

--- Algorithms from lectures/Graph algorithms/topological_sorting.py
+++ Algorithms from lectures/Graph algorithms/topological_sorting.py
@@ -1,33 +1,40 @@
-# Cały algorytm sortowania topologicznego polega na wywołaniu DFS-a z wybranego wierzchołka w grafie (źródła).
-# W momencie kiedy jest on przetwarzany, dopisujemy go na początek nowo tworzonej listy, ponieważ mamy zagwarantowane,
-# że żadna nowa krawędź z tego wierzchołka nie będzie wychodzić, która ewentualnie psułaby nasz porządek wierzchołków.
-# Zmienną z wartością nonlocal można idealnie użyć w wypadku funkcji rekurencyjnych tak, aby wartość ta nie była
-# modyfikowana przez powroty rekurencyjne np. zmienna "time" albo zbiór "set" w tym zadaniu.
-# Warto także zauważyć, iż tablica processed to sortowania topologicznego nie jest w ogóle konieczna, ponieważ
-# interesuje nas tylko fakt, że jeżeli wierzchołek został przetworzony to można go do zbioru dodać.
-# Złożoność czasowa: O(V+E)
-# Złożoność pamięciowa: O(V) - tablice visited i set, która zawiera posortowane wierzchołki
-from math import inf
-
-
-def topological_sorting(tab):
-    n = len(tab)
+# [ENG] Topological sorting means finding specific order of vertices for which, if we construct graph, edges will be
+# only from left to right, so we can perform it only in dags (directed acyclic graphs). Whole algorithm goes on using
+# DFS. If certain vertex is being processed we are adding it at the end of new created list. At the end, we reverse
+# that list. This algorithm works, because when certain vertex is processed we have a certainty that any other edge
+# doesn't start from that vertex (that could go to other vertex, which is not processed).
+# Time complexity: same as DFS
+# Space complexity: O(V) - "visited" array for marking already visited vertices ("processed" array is not necessary)
+# and array for solution
+# [PL] Sortowanie topologiczne polega na znalezieniu takiej kolejności wierzchołków w grafie, dla której jeśli
+# skonstruowalibyśmy graf, to krawędzie łączyłyby wierzchołki od lewej do prawej, a zatem możemy tego dokonać
+# tylko w dagach (skierowanych grafach acyklicznych). Cały algorytm sortowania topologicznego polega na wywołaniu
+# DFS-a z wybranego wierzchołka w grafie (źródła). W momencie kiedy jest on przetwarzany, dopisujemy go
+# na początek nowo tworzonej listy, ponieważ mamy zagwarantowane, że żadna nowa krawędź z tego wierzchołka
+# nie będzie wychodzić, która ewentualnie psułaby nasz porządek wierzchołków i prowadziła do 
+# wierzchołka, który jeszcze przetworzony nie był. Warto także zauważyć, iż tablica "processed" do sortowania
+# topologicznego nie jest w ogóle konieczna, ponieważ interesuje nas tylko fakt, że jeżeli wierzchołek został
+# przetworzony to można go do zbioru dodać.
+# Złożoność czasowa: taka sama jak DFS
+# Złożoność pamięciowa: O(V) - tablice "visited" dla zaznaczania już odwiedzonych wierzchołków oraz tablica dla
+# rozwiązania.
+def topological_sorting(graph):
+    n = len(graph)
     set = []
     visited = [0]*n
-
-    def dfs_visit(tab, s):
-        nonlocal set
-        visited[s] = 1
-        for x in range(len(tab[s])):
-            if visited[tab[s][x]] == 0:
-                dfs_visit(tab, tab[s][x])
-        set.append(s)
-
     for x in range(n):
         if visited[x] == 0:
-            dfs_visit(tab, x)
+            dfs_visit(graph, set, visited, x)
     return set[::-1]
 
 
-tab = [[1, 2], [5, 4], [3], [6], [6], [6], []]
-print(topological_sorting(tab))
+def dfs_visit(graph, set, visited, s):
+    visited[s] = 1
+    for x in range(len(graph[s])):
+        if visited[graph[s][x]] == 0:
+            dfs_visit(graph, set, visited, graph[s][x])
+    set.append(s)
+
+
+graph = [[1, 2], [5, 4], [3], [1, 6], [6], [6], []]
+print(topological_sorting(graph))
