17b5c4f350eaa735f4dd050679dfcc6a367594d2
ldydek <91955435+ldydek@users.noreply.github.com>
2022-02-14 13:21:57 +0100

    Update binary_search.py

--- Algorithms from lectures/Divide and conquer/binary_search.py
+++ Algorithms from lectures/Divide and conquer/binary_search.py
@@ -4,6 +4,11 @@
 # Złożność pamięciowa: iteracyjnie - O(1), rekurencyjnie - O(lg n) w powodu odkładania funkcji rekurencyjnych na
 # stosie.
 
+# Binary search algorithm. Watch out! In a given array has to be an order preserved. Otherwise, we won't be able to find
+# a certain element faster than linear search of the entire array.
+# Time complexity: O(lg n)
+# Space complexity: iteratively - O(1), recursively - O(lg n)
+
 
 def binary_search(tab, k):
     n = len(tab)
