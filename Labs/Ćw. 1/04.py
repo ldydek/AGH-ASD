97b237fe3d43b3c7e6e10fc496a744e50db74d91
ldydek <91955435+ldydek@users.noreply.github.com>
2022-02-20 20:54:26 +0100

    Add files via upload

---
+++ Labs/Ćw. 1/04.py
@@ -0,0 +1,19 @@
+def min_max(tab):
+    n = len(tab)
+    if n == 1:
+        return tab[0], tab[0]
+    if n % 2 == 0:
+        minimum = min(tab[0], tab[1])
+        maximum = max(tab[0], tab[1])
+    else:
+        minimum = tab[0]
+        maximum = tab[0]
+    for x in range(2-n % 2, n, 2):
+        a, b = min(tab[x], tab[x+1]), max(tab[x], tab[x+1])
+        minimum = min(minimum, a)
+        maximum = max(maximum, b)
+    return minimum, maximum
+
+
+tab = [2, 10, 4, 7, -4, 300, 40, 5]
+print(min_max(tab))
