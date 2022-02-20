51e79b803197a557ea475c685b9bcde8bb19b91f
ldydek <91955435+ldydek@users.noreply.github.com>
2022-02-20 15:28:28 +0100

    Add files via upload

---
+++ Labs/Ćw. 1/01.py
@@ -0,0 +1,11 @@
+def bubble_sort(tab):
+    n = len(tab)
+    for x in range(n):
+        for y in range(n-1):
+            if tab[y] > tab[y+1]:
+                tab[y], tab[y+1] = tab[y+1], tab[y]
+    return tab
+
+
+tab = [10, 2, 13, 56, 14]
+print(bubble_sort(tab))
