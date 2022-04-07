21cd3ef8f752397dea6d39ba645c583cd565daed
ldydek <91955435+ldydek@users.noreply.github.com>
2022-04-07 12:02:21 +0200

    Update zad2.py

--- Labs/Ćw. 5/zad2.py
+++ Labs/Ćw. 5/zad2.py
@@ -2,8 +2,6 @@
 # f(i, j) = f(i-1, j) or f(i-1, j-A[i]) - albo dany element bierzemy do sumy, albo nie
 
 def sum_1(tab, t):
-    if t < 0:
-        return 0
     n = len(tab)
     aux_tab = [[0 for _ in range(t+1)] for _ in range(n)]
     if tab[0] < t:
@@ -13,8 +11,8 @@ def sum_1(tab, t):
 
     for x in range(1, n):
         for y in range(1, t+1):
-            aux_tab[x][y] = aux_tab[x][y] or aux_tab[x-1][y]
-            if y-tab[x] >= 0:
+            aux_tab[x][y] = aux_tab[x-1][y]
+            if y >= tab[x]:
                 aux_tab[x][y] = aux_tab[x-1][y] or aux_tab[x-1][y-tab[x]]
     return aux_tab[n-1][t]
 
