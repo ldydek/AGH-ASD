cd0baec4f7d72f58681cb588d1a19751a67dd499
ldydek <91955435+ldydek@users.noreply.github.com>
2022-04-28 11:00:04 +0200

    Update 01_1.py

--- Labs/Ćw. 8/01_1.py
+++ Labs/Ćw. 8/01_1.py
@@ -2,8 +2,8 @@
 # considering a certain vertex there are two cases: either its neighbour is not visited and we can mark this vertex
 # as a part of a second set or it was already visited and it has to be already a part of a second set. If certain vertex
 # and one of its neighbours have the same number that classifies them to the same set it means that this graph is not
-# bipartite. If this situation won't happen graph is bipartite. Additionally I suppose here that given graph don't have
-# to be consistent.
+# bipartite. If this situation won't happen graph is bipartite. Additionally, I suppose here that given graph doesn't 
+# have to be consistent.
 
 from collections import deque
 
