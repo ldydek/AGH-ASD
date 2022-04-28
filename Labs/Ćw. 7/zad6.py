df06990d2a5b7439fad007d260668a556e934efa
ldydek <91955435+ldydek@users.noreply.github.com>
2022-04-28 15:07:08 +0200

    Add files via upload

---
+++ Labs/Ćw. 7/zad6.py
@@ -0,0 +1,7 @@
+# Zadanie 6. (suma odległości) Dana jest posortowana tablica A zawierająca n liczb i celem jest wyznaczenie liczby x
+# takiej, że wartość ∑|A[i]-x| jest minimalna. Proszę zaproponować algorytm, uzasadnić jego poprawność
+# oraz ocenić złożoność obliczeniową.
+#
+#
+# Znajduję miedianę tego zbioru po prostu przez odczytanie odpowiedniego indeksu, ponieważ tablica jest
+# posortowana i ona spełnia warunki zadania, ponieważ jest niewiększa i niemniejsza od połowy elementów tablicy.
