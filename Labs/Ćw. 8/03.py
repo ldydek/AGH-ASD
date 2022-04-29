5d5c6863be6af0e38a31a3653b07e369cc0cf87b
ldydek <91955435+ldydek@users.noreply.github.com>
2022-04-29 16:06:57 +0200

    Add files via upload

---
+++ Labs/Ćw. 8/03.py
@@ -0,0 +1,34 @@
+# Klasyczny BFS z tablicą "parent", dzięki której można skonstruuować najkrótsze ścieżki z zadanego źródła.
+
+from math import inf
+from collections import deque
+
+
+def print_path(parent, s, t):
+    solution = []
+    while t != -1:
+        solution.append(t)
+        t = parent[t]
+    return solution[::-1]
+
+
+def bfs(tab, s, t):
+    n = len(tab)
+    distance = [None]*n
+    parent = [inf]*n
+    distance[s] = 0
+    parent[s] = -1
+    Q = deque()
+    Q.append(s)
+    while Q:
+        u = Q.popleft()
+        for x in range(len(tab[u])):
+            if distance[tab[u][x]] is None:
+                distance[tab[u][x]] = distance[u] + 1
+                parent[tab[u][x]] = u
+                Q.append(tab[u][x])
+    return print_path(parent, s, t)
+
+
+tab = [[1, 2], [0], [0, 3, 5], [2, 4, 5], [3], [2, 3]]
+print(bfs(tab, 0, 4))
