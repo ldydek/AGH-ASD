ac55fce1522c4a15edbdeedfd2fe9f14937e4c05
ldydek <91955435+ldydek@users.noreply.github.com>
2022-06-02 10:34:19 +0200

    Update 02.py

--- Labs/Ćw. 10/02.py
+++ Labs/Ćw. 10/02.py
@@ -1,7 +1,7 @@
 # We can create transitive closure of a given graph using Floyd-Warshall algorithm. If we suppose that adjacency matrix
 # has only 0-1 values we can get a transitive closure without need to allocate second matrix. Using dynamic programming
 # and logical operators we can create an edge from, for instance, (u,v) if for certain "k" there were edges (u,k) and
-# (k,v) (path from "u" to "k" and from "k" to "v" in an input graph).
+# (k,v) (paths from "u" to "k" and from "k" to "v" in an input graph).
 # Time complexity: O(V^3)
 # Space complexity: O(1)
 
