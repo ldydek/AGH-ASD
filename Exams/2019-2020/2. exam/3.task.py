a7ae84ab5cd307cf6eb6a58e1b8f786b12a63418
ldydek <91955435+ldydek@users.noreply.github.com>
2022-07-05 19:28:39 +0200

    Update 3.task.py

--- Exams/2019-2020/2. exam/3.task.py
+++ Exams/2019-2020/2. exam/3.task.py
@@ -1,6 +1,7 @@
 # Idea is to from given matrix create a directed acyclic graph (dag). In this graph edge (u,v) informs us that task "u"
 # has to be done before task "v". At the end, we can sort given graph topologically and obtain correct sequence of
-# vertices.
+# vertices. Note that allocating another matrix for a graph is not necessary, because when given matrix as an input
+# has 1 value edge is in correct direction. We have to only change edge direction, when a certain matrix field has 2.
 # Time complexity: O(n^2)
 # Space complexity: O(n) - recursion
 # Passed all tests
