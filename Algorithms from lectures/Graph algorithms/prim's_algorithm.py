af0e2cc7a0d5fdd5b72714a7a64a6e8059cbcfb2
ldydek <91955435+ldydek@users.noreply.github.com>
2022-07-05 15:44:12 +0200

    Update prim's_algorithm.py

--- Algorithms from lectures/Graph algorithms/prim's_algorithm.py
+++ Algorithms from lectures/Graph algorithms/prim's_algorithm.py
@@ -2,7 +2,9 @@
 # Time complexity: O(V^2)
 # Space complexity: O(V)
 
-def prim_algorithm(graph, s):
+import heapq
+
+def prim_algorithm_m(graph, s):
     n = len(graph)
     distance = [float("inf") for _ in range(n)]
     distance[s] = float("-inf")
@@ -26,6 +28,29 @@ def prim_algorithm(graph, s):
     return solution
 
 
+def prim_algorithm_l(graph, s):
+    n = len(graph)
+    heap = [(0, s)]
+    selected = [0] * n
+    parent = [-1] * n
+    values = [float("inf")] * n
+    solution = []
+
+    while heap:
+        dist, u = heapq.heappop(heap)
+        selected[u] = 1
+        for y, z in graph[u]:
+            if selected[y] == 0 and values[y] > z:
+                values[y] = z
+                parent[y] = u
+                heapq.heappush(heap, (values[y], y))
+    for x in range(n):
+        if parent[x] == -1:
+            continue
+        solution.append((parent[x], x))
+    return solution
+
+
 graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
          [4, 0, 8, 0, 0, 0, 0, 11, 0],
          [0, 8, 0, 7, 0, 4, 0, 0, 2],
@@ -35,4 +60,4 @@ graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
          [0, 0, 0, 0, 0, 2, 0, 1, 6],
          [8, 11, 0, 0, 0, 0, 1, 0, 7],
          [0, 0, 2, 0, 0, 0, 6, 7, 0]]
-print(prim_algorithm(graph, 0))
+print(prim_algorithm_m(graph, 0))
