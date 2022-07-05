7f8a9554162b0bcfb8f89cb1a69dcf72d42648ad
ldydek <91955435+ldydek@users.noreply.github.com>
2022-07-05 13:44:36 +0200

    Update merge_sort.py

--- Algorithms from lectures/Sorting algorithms/merge_sort.py
+++ Algorithms from lectures/Sorting algorithms/merge_sort.py
@@ -9,48 +9,43 @@
 # Złożoność pamięciowa: O(n)
 
 def merge_sort(tab):
-    def reku(tab, p, r):
-        if p < r:
-            q = (p+r)//2
-            reku(tab, p, q)
-            reku(tab, q+1, r)
-            merge(tab, aux_tab, p, q, r)
-        return tab
-
-    def merge(tab, aux_tab, p, q, r):
-        n1 = q - p + 1
-        n2 = r - q
-        a, b = p, q
-        xd1, xd2 = 0, 0
-        ctr = a
-        while xd1 < n1 and xd2 < n2:
-            if tab[a] < tab[b+1]:
-                aux_tab[ctr] = tab[a]
-                xd1 += 1
-                ctr += 1
-                a += 1
-            else:
-                aux_tab[ctr] = tab[b+1]
-                xd2 += 1
-                ctr += 1
-                b += 1
-        while xd1 < n1:
-            aux_tab[ctr] = tab[a]
-            xd1 += 1
-            ctr += 1
-            a += 1
-        while xd2 < n2:
-            aux_tab[ctr] = tab[b+1]
-            xd2 += 1
-            ctr += 1
-            b += 1
-        for x in range(p, r+1):
-            tab[x] = aux_tab[x]
-
     n = len(tab)
-    aux_tab = [None]*n
-    reku(tab, 0, n-1)
+    aux_tab = [0] * n
+    divide_and_conquer(tab, 0, n-1, aux_tab)
     return tab
+
+
+def divide_and_conquer(tab, p, r, aux_tab):
+    q = (p+r)//2
+    if p == r:
+        return
+    divide_and_conquer(tab, p, q, aux_tab)
+    divide_and_conquer(tab, q+1, r, aux_tab)
+    merge(tab, aux_tab, p, r)
+
+
+def merge(tab, aux_tab, p, r):
+    q = (p+r)//2
+    xd1, xd2 = p, q + 1
+    ctr = p
+    while xd1 <= q and xd2 <= r:
+        if tab[xd1] <= tab[xd2]:
+            aux_tab[ctr] = tab[xd1]
+            xd1 += 1
+        else:
+            aux_tab[ctr] = tab[xd2]
+            xd2 += 1
+        ctr += 1
+    while xd1 <= q:
+        aux_tab[ctr] = tab[xd1]
+        ctr += 1
+        xd1 += 1
+    while xd2 <= r:
+        aux_tab[ctr] = tab[xd2]
+        ctr += 1
+        xd2 += 1
+    for x in range(p, r+1):
+        tab[x] = aux_tab[x]
 
 
 tab = [4, 3, 1, 9, 3, 5, 10, 15, 2, 34, 2, 14]
